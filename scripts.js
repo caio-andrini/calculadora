6d8685adb10c7a5719a26fa6dd7e52d13f591898
caio <andrini.caio@escola.pr.gov.br>
2026-06-26 21:51:44 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -11,3 +11,9 @@ function back () {
     var resultado = document.getElementById('resultado').innerHTML ;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length-1) ;
 }
+function calcular () {
+    var resultado = document.getElementById('resultado').innerHTML ;
+    if ( resultado ) {
+        document.getElementById('resultado').innerHTML = eval(resultado) ;
+    }
+}
